ec542efcc2262c8b51277d52065cf7c1fd07c169
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-05-05 16:18:20 +0300

    replace int to slug in posts and categories

--- women/models.py
+++ women/models.py
@@ -9,19 +9,20 @@ class Women(models.Model):
         ordering = ['-time_create', 'title']
 
     title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(blank=True, verbose_name='Текст статьи')
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name='Фото')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
-    cat = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, verbose_name='Категория')
+    cat = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
 
     def __str__(self):
         return self.title
 
     #  Автоматически создает в админке кнопку "Посмотреть на сайте"
     def get_absolute_url(self):
-        return reverse('post', kwargs={'post_id': self.pk})
+        return reverse('post', kwargs={'post_slug': self.slug})
 
 
 class Category(models.Model):
@@ -31,9 +32,10 @@ class Category(models.Model):
         ordering = ('id',)
 
     name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
     def get_absolute_url(self):
-        return reverse('category', kwargs={'cat_id': self.pk})
+        return reverse('category', kwargs={'cat_slug': self.slug})
 
     def __str__(self):
         return self.name
--- women/views.py
+++ women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import *
 
@@ -41,12 +41,19 @@ def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+    }
+    return render(request, 'women/post.html', context=context)
 
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id, is_published=True)
+def show_category(request, cat_slug):
+    posts = Women.objects.filter(cat__slug=cat_slug, is_published=True)
 
     if not posts:
         raise Http404()
@@ -54,8 +61,8 @@ def show_category(request, cat_id):
     context = {
         'posts': posts,
         'menu': menu,
-        'title': f'Рубрика №{cat_id}',
-        'cat_selected': cat_id
+        'title': f'Рубрика {cat_slug}',
+        'cat_selected': cat_slug
     }
 
     return render(request, 'women/index.html', context=context)
